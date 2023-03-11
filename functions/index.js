ef44290a7aa215efe5ae5c7fbc57088564b2bf98
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-03-10 23:09:54 -0500

    Add firebase functions express

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use(bodyParser.urlencoded({ extended: false }));
+const allowedOrigins = ['*',
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://serenity-journal.web.app'];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+
+}));
+app.get('/', (req, res) => {
+    res.send(`
+      <!doctype html>
+      <body>
+        <p>Hello World!</p>
+      </body>
+    </html>`);
+});
+
+exports.app = functions.https.onRequest(app);
