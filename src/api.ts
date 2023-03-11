db583b466c70d90bfd51177aba15312da6d2c558
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-03-10 23:27:47 -0500

    Endpoint to create new journal DBO (#3)

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,2 @@
-// @ts-nocheck
-export const API_URL = process.env.NODE_ENV === 'production' ? 'https://us-central1-serenity-journal.cloudfunctions.net/app' : 'http://127.0.0.1:5001/serenity-journal/us-central1/app';
+export const API_URL = import.meta.env.PROD ? 'https://us-central1-serenity-journal.cloudfunctions.net/app' : 'http://127.0.0.1:5001/serenity-journal/us-central1/app';
+console.log("API URL: ", API_URL);
