4f28bcd65b833952b0c907a6a419d575963f0a98
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-03-10 23:10:22 -0500

    Add express server in firebase functions (#2)

---
+++ src/api.ts
@@ -0,0 +1,2 @@
+// @ts-nocheck
+export const API_URL = process.env.NODE_ENV === 'production' ? 'https://us-central1-serenity-journal.cloudfunctions.net/app' : 'http://127.0.0.1:5001/serenity-journal/us-central1/app';
